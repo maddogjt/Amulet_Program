c4dcc6ea6214b947c2b53f7c3055f296c7f9bb70
Tommy <4096910+TommyBarnes@users.noreply.github.com>
2022-07-14 00:06:56 -0700

    update manufacturer id

--- amulet/src/communication/parameters.h
+++ amulet/src/communication/parameters.h
@@ -2,7 +2,8 @@
 
 // You are supposed to get manufacturer ids from the bluetooth consortium but I made one up
 
-static constexpr uint16_t kAmuletManufacturerId = 0x69AA;  // Used for Critial 2019 amulets
+static constexpr uint16_t kAmuletManufacturerId = 0x69AB;  // Used for Critial 2022 amulets
+// static constexpr uint16_t kAmuletManufacturerId = 0x69AA;  // Used for Critial 2019 amulets
 //static constexpr uint16_t kAmuletManufacturerId = 0x7A3D;  // Used for Burn 2019 amulets
 
 static constexpr uint8_t kAmuletDataVersion = 1;
